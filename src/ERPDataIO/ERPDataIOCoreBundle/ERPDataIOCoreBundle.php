41eddff449d0884aa78080132abef61b3c2c3c21
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-07-24 13:03:16 +0200

    Create events definition.

---
+++ src/ERPDataIO/ERPDataIOCoreBundle/ERPDataIOCoreBundle.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * ERPDataIOCoreBundle for Elcodi
+ *
+ * This Bundle is part of ERPDataIO bridge for Elcodi
+ *
+ * @author Arkaitz Garro <arkaitz.garro@gmail.com>
+ * @package ERPDataIOCoreBundle
+ *
+ * Arkaitz Garro 2014
+ */
+
+namespace ERPDataIO\ERPDataIOCoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * This class define all events thrown by all import/export methods
+ */
+class ERPDataIOCoreEvents
+{
+    /**
+     * This event is thrown when data import starts
+     *
+     * event.name : erpdataio.import.start
+     * event.class : ERPDataIOImportStartEvent
+     */
+    const ERPDATAIO_IMPORT_START = 'erpdataio.import.start';
+
+    /**
+     * This event is thrown when data import ends
+     *
+     * event.name : erpdataio.import.end
+     * event.class : ERPDataIOImportEndEvent
+     */
+    const ERPDATAIO_IMPORT_END = 'erpdataio.import.end';
+
+    /**
+     * This event is thrown when data import fails for any reason
+     *
+     * event.name : erpdataio.import.fail
+     * event.class : ERPDataIOImportFailEvent
+     */
+    const ERPDATAIO_IMPORT_FAIL = 'erpdataio.import.fail';
+
+    /**
+     * This event is thrown when data import ends
+     *
+     * event.name : erpdataio.import.end
+     * event.class : ERPDataIOImportStartEvent
+     */
+    const ERPDATAIO_IMPORT_START = 'erpdataio.import.start';
+
+    /**
+     * This event is thrown when data import ends
+     *
+     * event.name : erpdataio.import.end
+     * event.class : ERPDataIOImportEndEvent
+     */
+    const ERPDATAIO_IMPORT_END = 'erpdataio.import.end';
+
+    /**
+     * This event is thrown when data import fails for any reason
+     *
+     * event.name : erpdataio.import.fail
+     * event.class : ERPDataIOImportFailEvent
+     */
+    const ERPDATAIO_IMPORT_FAIL = 'erpdataio.import.fail';
+}
---
+++ src/ERPDataIO/ERPDataIOCoreBundle/ERPDataIOCoreEvents.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * ERPDataIOCoreBundle for Elcodi
+ *
+ * This Bundle is part of ERPDataIO bridge for Elcodi
+ *
+ * @author Arkaitz Garro <arkaitz.garro@gmail.com>
+ * @package ERPDataIOCoreBundle
+ *
+ * Arkaitz Garro 2014
+ */
+
+namespace ERPDataIO\ERPDataIOCoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Core ERPDataIO Bundle
+ */
+class ERPDataIOCoreBundle extends Bundle
+{
+}
