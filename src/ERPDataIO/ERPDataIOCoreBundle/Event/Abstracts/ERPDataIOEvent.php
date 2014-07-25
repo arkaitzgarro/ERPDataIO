3f4a73a3add4affb36a2e84f60b48a824d6673d5
Arkaitz Garro <arkaitz.garro@gmail.com>
2014-07-25 11:54:14 +0200

    Create base event class

---
+++ src/ERPDataIO/ERPDataIOCoreBundle/Event/Abstracts/ERPDataIOEvent.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * ERPDataIOCoreBundle for Elcodi
+ *
+ * This Bundle is part of ERPDataIO bridge for Elcodi
+ *
+ * @author Arkaitz Garro <arkaitz.garro@gmail.com>
+ * @package ERPDataIOCoreBundle
+ *
+ * Arkaitz Garro 2014
+ */
+
+namespace ERPDataIO\ERPDataIOCoreBundle\Abstracts;
+
+use Symfony\Component\EventDispatcher\Event;
+
+/**
+ * Abstract data import/export event
+ */
+abstract class ERPDataIOEvent extends Event
+{
+
+}
